d8f8c01d4eb38e6e85d924d18ca45eeb335bf946
MaryEddythe <sornito.maryeddythe@gmail.com>
2024-04-29 02:31:51 +0800

    remove icon emoji button

--- lib/page/chatpage.dart
+++ lib/page/chatpage.dart
@@ -35,8 +35,8 @@ class _ChatPageState extends State<ChatPage> {
     myFocusNode.addListener(() {
       if (myFocusNode.hasFocus) {
         Future.delayed(
-          const Duration(milliseconds: 500), 
-          () => scrollDown(),
+          const Duration(milliseconds: 500),
+              () => scrollDown(),
         );
       }
     });
@@ -51,7 +51,8 @@ class _ChatPageState extends State<ChatPage> {
 
   void sendMessage() async {
     if (_messageController.text.isNotEmpty) {
-      await _chatService.sendMessage(widget.receiverID, _messageController.text);
+      await _chatService.sendMessage(
+          widget.receiverID, _messageController.text);
       _messageController.clear();
     }
     scrollDown();
@@ -59,28 +60,31 @@ class _ChatPageState extends State<ChatPage> {
 
   void scrollDown() {
     _scrollController.animateTo(
-      _scrollController.position.maxScrollExtent, 
-      duration: const Duration(seconds: 1), 
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(seconds: 1),
       curve: Curves.fastOutSlowIn,
     );
   }
 
   void handleChatBubbleClick(int index) {
-  setState(() {
-    if (lastClickedIndex == index) {
-      // If the clicked message is the same as the last clicked one, reset lastClickedIndex
-      lastClickedIndex = -1;
-    } else {
-      // Otherwise, set lastClickedIndex to the clicked index
-      lastClickedIndex = index;
-    }
-  });
-}
+    setState(() {
+      if (lastClickedIndex == index) {
+        // If the clicked message is the same as the last clicked one, reset lastClickedIndex
+        lastClickedIndex = -1;
+      } else {
+        // Otherwise, set lastClickedIndex to the clicked index
+        lastClickedIndex = index;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
+      backgroundColor: Theme
+          .of(context)
+          .colorScheme
+          .background,
       appBar: AppBar(
         title: Text(
           widget.receiverEmail,
@@ -122,7 +126,8 @@ class _ChatPageState extends State<ChatPage> {
           controller: _scrollController,
           itemCount: snapshot.data!.docs.length,
           itemBuilder: (context, index) {
-            return _buildMessageItem(snapshot.data!.docs[index], index, snapshot.data!.docs.length);
+            return _buildMessageItem(
+                snapshot.data!.docs[index], index, snapshot.data!.docs.length);
           },
         );
       },
@@ -130,107 +135,104 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   // Inside _buildMessageItem method
-Widget _buildMessageItem(DocumentSnapshot doc, int index, int totalCount) {
-  Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-  bool isCurrentUser = data['senderID'] == _authService.getCurrentUser()!.uid;
+  Widget _buildMessageItem(DocumentSnapshot doc, int index, int totalCount) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    bool isCurrentUser = data['senderID'] == _authService.getCurrentUser()!.uid;
 
-  // Parse the timestamp from the data
-  Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
-  DateTime messageDateTime = timestamp.toDate();
+    // Parse the timestamp from the data
+    Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
+    DateTime messageDateTime = timestamp.toDate();
 
-  // Get the current time
-  DateTime now = DateTime.now();
+    // Get the current time
+    DateTime now = DateTime.now();
 
-  // Calculate the difference in days
-  int differenceInDays = now.difference(messageDateTime).inDays;
+    // Calculate the difference in days
+    int differenceInDays = now
+        .difference(messageDateTime)
+        .inDays;
 
-  // Format the time based on the 12-hour clock format
-  String timeString = DateFormat.jm().format(messageDateTime); 
+    // Format the time based on the 12-hour clock format
+    String timeString = DateFormat.jm().format(messageDateTime);
 
-  // Check if this is the last message or if it matches the last clicked index
-  bool shouldShowTime = index == totalCount - 1 || index == lastClickedIndex;
+    // Check if this is the last message or if it matches the last clicked index
+    bool shouldShowTime = index == totalCount - 1 || index == lastClickedIndex;
 
-  // Check if the message was sent more than 24 hours ago
-  if (differenceInDays > 1) {
-    // Show the day of the week if the message was sent more than 24 hours ago
-    timeString = DateFormat('EEEE').format(messageDateTime);
-  } else if (differenceInDays > 7) {
-    // Show the month and date if the message was sent more than 7 days ago
-    timeString = DateFormat('MMM d').format(messageDateTime);
+    // Check if the message was sent more than 24 hours ago
+    if (differenceInDays > 1) {
+      // Show the day of the week if the message was sent more than 24 hours ago
+      timeString = DateFormat('EEEE').format(messageDateTime);
+    } else if (differenceInDays > 7) {
+      // Show the month and date if the message was sent more than 7 days ago
+      timeString = DateFormat('MMM d').format(messageDateTime);
+    }
+
+    return GestureDetector(
+      onLongPress: () {
+        _messageController.text = '${data["message"]} ';
+        myFocusNode.requestFocus();
+      },
+      child: Column(
+        crossAxisAlignment: isCurrentUser
+            ? CrossAxisAlignment.end
+            : CrossAxisAlignment.start,
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            transform: lastClickedIndex == index ? Matrix4.translationValues(
+                0, -5, 0) : Matrix4.translationValues(0, 0, 0),
+            child: ChatBubble(
+                message: data["message"], isCurrentUser: isCurrentUser),
+          ),
+          if (shouldShowTime)
+            Text(
+              timeString,
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 12,
+              ),
+            ),
+        ],
+      ),
+    );
   }
 
-  return GestureDetector(
-    onLongPress: () {
-      _messageController.text = '${data["message"]} ';
-      myFocusNode.requestFocus();
-    },
-    child: Column(
-      crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-      children: [
-        AnimatedContainer(
-          duration: Duration(milliseconds: 500),
-          transform: lastClickedIndex == index ? Matrix4.translationValues(0, -5, 0) : Matrix4.translationValues(0, 0, 0),
-          child: ChatBubble(message: data["message"], isCurrentUser: isCurrentUser),
-        ),
-        if (shouldShowTime) 
-          Text(
-            timeString,
-            style: const TextStyle(
-              color: Colors.grey,
-              fontSize: 12,
-            ),
-          ),
-      ],
-    ),
-  );
-}
-
-
- Widget _buildUserInput() {
-  return Padding(
-    padding: const EdgeInsets.only(top: 10, bottom: 50.0, left: 15, right: 0),
-    child: Row(
-      children: [
-        IconButton(
-          onPressed: () {
-            // Add your logic to handle emoji button press
-          },
-          icon: const Icon(
-            Icons.emoji_emotions, // Use any emoji icon you prefer
-            color: Color(0xff5072A7),
-          ),
-        ),
-        Expanded(
-          child: Stack(
-            children: [
-              MyTextField(
-                controller: _messageController,
-                hintText: "Type a message",
-                obscureText: false,
-                focusNode: myFocusNode,
-              ),
-              Positioned(
-                right: 40, // Adjust this value as needed to move the button more to the left
-                bottom: 7,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    //color: Color(0xff5072A7),
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    onPressed: sendMessage,
-                    icon: const Icon(
-                      Icons.send,
-                      color: Color(0xff5072A7),
+  Widget _buildUserInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 50.0, left: 15, right: 0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                MyTextField(
+                  controller: _messageController,
+                  hintText: "Type a message",
+                  obscureText: false,
+                  focusNode: myFocusNode,
+                ),
+                Positioned(
+                  right: 40,
+                  // Adjust this value as needed to move the button more to the left
+                  bottom: 7,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      //color: Color(0xff5072A7),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: sendMessage,
+                      icon: const Icon(
+                        Icons.send,
+                        color: Color(0xff5072A7),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 }
